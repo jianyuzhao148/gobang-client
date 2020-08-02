6eb7b426cb11babc1ed5ebc4a9300b46f8b532f0
zhaojyu <zhaojyu@163.com>
2020-08-03 00:54:51 +0800

    fourth commit

--- libs/exml.e.d.ts
+++ libs/exml.e.d.ts
@@ -6,7 +6,9 @@ declare module skins{
 	class CheckBoxSkin extends eui.Skin{
 	}
 }
-declare class LoadSceneSkin extends eui.Skin{
+declare class LoadPanelSkin extends eui.Skin{
+}
+declare class UserInfoPanelSkin extends eui.Skin{
 }
 declare module game{
 	class exmlSkin extends eui.Skin{
@@ -34,11 +36,15 @@ declare class CreateButtonSkin extends eui.Skin{
 }
 declare class FastGameButtonSkin extends eui.Skin{
 }
+declare class MailButtonSkin extends eui.Skin{
+}
 declare class RoomListItemSkin extends eui.Skin{
 }
 declare class roomListSkin extends eui.Skin{
 }
 declare class SearchButtonSkin extends eui.Skin{
+}
+declare class TaskButtonSkin extends eui.Skin{
 }
 declare module skins{
 	class ProgressBarSkin extends eui.Skin{
